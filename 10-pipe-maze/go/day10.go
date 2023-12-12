f6e92d5f6724646f6bf5ce362ebb71e8c6ca28ea
Stephan Deininghaus <stephan@deininghaus.com>
2023-12-12 11:08:33 +0000

    Start Go solution for Dec 10 puzzle (pipe maze)

---
+++ 10-pipe-maze/go/day10.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+)
+
+type Maze = [][]byte
+
+func findStartPos(maze Maze) (int, int) {
+	width, height := len(maze[0]), len(maze)
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			if maze[i][j] == 'S' {
+				return i, j
+			}
+		}
+	}
+
+	panic("No starting point found.")
+}
+
+const (
+	North = 1 << iota
+	South = 1 << iota
+	East  = 1 << iota
+	West  = 1 << iota
+)
+
+var connections = map[byte]int{
+	'|': North | South,
+	'-': East | West,
+	'L': North | East,
+	'J': North | West,
+	'7': South | West,
+	'F': South | East,
+	'.': 0,
+}
+
+func main() {
+	data, _ := os.ReadFile("sample.txt")
+	maze := bytes.Split(data, []byte("\n"))
+
+	i_0, j_0 := findStartPos(maze)
+	fmt.Println(i_0, j_0, connections['-'])
+}
---
+++ 10-pipe-maze/go/go.mod
@@ -0,0 +1,3 @@
+module aoc-2023/day10
+
+go 1.21.3
